d10b6f1aaafa8a9a74f81df3574eeacae9f74fa8
Rakodashi <rafaconrado1@gmail.com>
2023-06-02 00:32:49 -0300

    Fixing hamburger menu's functionality

--- main.js
+++ main.js
@@ -3,3 +3,16 @@
 function changeTheme() {
     body.classList.toggle('light');
 };
+
+// nav animation for hamburger menu
+
+const navMenu = document.getElementsByClassName('.hamburgerMenu'),
+toggleMenu = document.getElementById('menu-btn')
+closeMenu = document.getElementById('menu-btn')
+
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+})
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
