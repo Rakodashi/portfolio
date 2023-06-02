c032c9fe2a986307dba858c595c1cf48a1b75f03
Rakodashi <rafaconrado1@gmail.com>
2023-06-02 00:46:59 -0300

    removed test comments

--- main.js
+++ main.js
@@ -13,6 +13,6 @@ closeMenu = document.getElementById('menu-btn')
 toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
-// closeMenu.addEventListener('click', () => {
-//     navMenu.classList.remove('show')
-// })
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
