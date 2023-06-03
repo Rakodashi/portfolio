f8f11dc6a87baf52ffed32533c0ff918c96dce06
Rakodashi <rafaconrado1@gmail.com>
2023-06-03 19:46:45 -0300

    Added hamburger menu functionality

--- main.js
+++ main.js
@@ -4,15 +4,7 @@ function changeTheme() {
     body.classList.toggle('light');
 };
 
-// nav animation for hamburger menu
-
-// const navMenu = document.getElementsById('.hamburgerMenu'),
-// toggleMenu = document.getElementById('menu-btn', 'menu-icon')
-
-// toggleMenu.addEventListener('click', () => {
-//     navMenu.classList.toggle('show')
-// })
-
+// Toggles hamburger menu opening and closing
 
 function toggleMenu() {
     body.classList.toggle('show');
